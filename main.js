3b5236c60846315cf6376c0e3321a40e40287daf
TheManiacMartian <61629906+TheManiacMartian@users.noreply.github.com>
2024-10-27 19:03:32 -0400

    Implemented electron

---
+++ main.js
@@ -0,0 +1,16 @@
+/** The main file that initializes electron. */
+const { app, BrowserWindow } = require('electron');
+
+/** Create new desktop window.
+ * @param width the width of the application window.
+ * @param height the height of the application window.
+ */
+function createWindow(width, height) {
+    const window = new BrowserWindow({width: width, height: height});
+    window.loadFile('dist/tourneypal/browser/index.html');
+}
+
+/** When the app is ready, we can create the window */
+app.whenReady().then(() => {
+    createWindow(1280, 720);
+})

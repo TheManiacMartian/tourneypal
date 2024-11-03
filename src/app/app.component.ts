b113747fec889ca78813084b9bbfb8b839355d49
TheManiacMartian <61629906+TheManiacMartian@users.noreply.github.com>
2024-11-02 22:23:10 -0400

    Install GraphQL

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
+import { MatSidenavContainer, MatSidenav, MatSidenavContent } from '@angular/material/sidenav'
+import { MatFabButton } from '@angular/material/button'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, NavComponent, RouterLink, RouterLinkActive, MatSidenavContainer, MatSidenav, MatSidenavContent, MatFabButton],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title = 'tourneypal';
+
+  toggleSidenav()
+  {
+    
+  }
 }
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { MatFabButton } from '@angular/material/button';
+import { MatActionList, MatListItem } from '@angular/material/list'
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatFabButton],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatFabButton, MatActionList, MatListItem],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
